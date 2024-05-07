d5307a3125d8ef45fd9035f446aed9aa673fc637
AtharvShirke9 <78665257+AtharvShirke9@users.noreply.github.com>
2024-05-07 12:40:05 +0530

    Delete client directory

--- client/src/services/http-common.js
+++
@@ -1,9 +0,0 @@
-// src/services/http-common.js
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
